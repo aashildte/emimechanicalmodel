0614be1bfae3aa5322743d873d22efbe1ea059b2
Åshild TElle <aashild@uw.edu>
2025-10-06 17:14:47 -0700

    minor updates in bash scripts for experiments

--- contraction_experiments.sh
+++ contraction_experiments.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 export OMP_NUM_THREADS=2
-mpirun -n 2 python scripts/contraction_fibrosis.py meshes_revisions/2d_mesh_baseline_10_0.h5 500 1 1 
+mpirun -n 2 python scripts/contraction_fibrosis.py meshes_fibrosis_paper/2d_mesh_baseline_10_0.h5 500 1 1 
 
 for seed in 1 2 3 4 5
 do
@@ -10,8 +10,8 @@ do
 		for fib in "replacement" "interstitial"
 		do
 	       		mesh="2d_mesh_${fib}_N_10_k_${k}_seed_${seed}.h5"
-			mpirun -n 2 python scripts/contraction_fibrosis.py meshes_revisions/$mesh 500 1 1 &
-			mpirun -n 2 python scripts/contraction_fibrosis.py meshes_revisions/$mesh 500 2 2
+			mpirun -n 2 python scripts/contraction_fibrosis.py meshes_fibrosis_paper/$mesh 500 1 1 &
+			mpirun -n 2 python scripts/contraction_fibrosis.py meshes_fibrosis_paper/$mesh 500 2 2
 		done
 	done
 done
@@ -24,16 +24,16 @@ do
 		mesh="2d_mesh_${fib}_N_10_k_${k}_seed_${seed}.h5"
 		for sc in 1.5 2 2.5 3 3.5 4
 		do
-			mpirun -n 2 python scripts/contraction_fibrosis.py meshes_revisions/$mesh 137 $sc 1
+			mpirun -n 2 python scripts/contraction_fibrosis.py meshes_fibrosis_paper/$mesh 137 $sc 1
 		done
 		for sc in 2 3 4 5 6 7 8
 		do
-			mpirun -n 2 python scripts/contraction_fibrosis.py meshes_revisions/$mesh 137 1 $sc
+			mpirun -n 2 python scripts/contraction_fibrosis.py meshes_fibrosis_paper/$mesh 137 1 $sc
 		done
 		
 		for sc in 3 4 5 6
 		do
-			mpirun -n 2 python scripts/contraction_fibrosis.py meshes_revisions/$mesh 137 $sc $sc
+			mpirun -n 2 python scripts/contraction_fibrosis.py meshes_fibrosis_paper/$mesh 137 $sc $sc
 		done
 	done
 done
--- stretch_experiments.sh
+++ stretch_experiments.sh
@@ -1,24 +1,25 @@
 #! /bin/bash
+export OMP_NUM_THREADS=1
 
-mpirun -n 2 python scripts/stretch_fibrosis.py meshes_revisions/2d_mesh_baseline_10_0.h5 stretch_ff 1 1 True 
-mpirun -n 2 python scripts/stretch_fibrosis.py meshes_revisions/2d_mesh_baseline_10_0.h5 stretch_ss 1 1 True 
-#export OMP_NUM_THREADS=1
+mpirun -n 2 python scripts/stretch_fibrosis.py meshes_fibrosis_paper/2d_mesh_baseline_10_0.h5 stretch_ff 1 1 True 
+mpirun -n 2 python scripts/stretch_fibrosis.py meshes_fibrosis_paper/2d_mesh_baseline_10_0.h5 stretch_ss 1 1 True 
 
-#for seed in 1 2 3 4 5
-#do
-#       	for k in 0 10
-#	do
-#		mesh="2d_mesh_interstitial_N_10_k_${k}_seed_${seed}.h5"
-#		mpirun -n 2 python scripts/stretch_fibrosis.py meshes_revisions/$mesh stretch_ff 1 1 False &
-#		mpirun -n 2 python scripts/stretch_fibrosis.py meshes_revisions/$mesh stretch_ss 1 1 False 
-#		mpirun -n 2 python scripts/stretch_fibrosis.py meshes_revisions/$mesh stretch_ff 2 2 False &
-#		mpirun -n 2 python scripts/stretch_fibrosis.py meshes_revisions/$mesh stretch_ss 2 2 False
-#		mesh="2d_mesh_replacement_N_10_k_${k}_seed_${seed}.h5"
-#		mpirun -n 2 python scripts/stretch_fibrosis.py meshes_revisions/$mesh stretch_ff 1 1 False &
-#		mpirun -n 2 python scripts/stretch_fibrosis.py meshes_revisions/$mesh stretch_ss 1 1 False 
-#		mpirun -n 2 python scripts/stretch_fibrosis.py meshes_revisions/$mesh stretch_ff 2 2 False &
-#		mpirun -n 2 python scripts/stretch_fibrosis.py meshes_revisions/$mesh stretch_ss 2 2 False
-#	done
+for seed in 1 2 3 4 5
+do
+       	for k in 0 10
+	do
+		mesh="2d_mesh_interstitial_N_10_k_${k}_seed_${seed}.h5"
+		mpirun -n 2 python scripts/stretch_fibrosis.py meshes_fibrosis_paper/$mesh stretch_ff 1 1 False &
+		mpirun -n 2 python scripts/stretch_fibrosis.py meshes_fibrosis_paper/$mesh stretch_ss 1 1 False 
+		mpirun -n 2 python scripts/stretch_fibrosis.py meshes_fibrosis_paper/$mesh stretch_ff 2 2 False &
+		mpirun -n 2 python scripts/stretch_fibrosis.py meshes_fibrosis_paper/$mesh stretch_ss 2 2 False
+		mesh="2d_mesh_replacement_N_10_k_${k}_seed_${seed}.h5"
+		mpirun -n 2 python scripts/stretch_fibrosis.py meshes_fibrosis_paper/$mesh stretch_ff 1 1 False &
+		mpirun -n 2 python scripts/stretch_fibrosis.py meshes_fibrosis_paper/$mesh stretch_ss 1 1 False 
+		mpirun -n 2 python scripts/stretch_fibrosis.py meshes_fibrosis_paper/$mesh stretch_ff 2 2 False &
+		mpirun -n 2 python scripts/stretch_fibrosis.py meshes_fibrosis_paper/$mesh stretch_ss 2 2 False
+	done
+done
 
 for seed in 1
 do
@@ -27,43 +28,43 @@ do
 		for sc in 2 3 4 5 6 7 8
 		do
 			mesh="2d_mesh_interstitial_N_10_k_${k}_seed_${seed}.h5"
-			#mpirun -n 2 python scripts/stretch_fibrosis.py meshes_revisions/$mesh stretch_ff 1 $sc False 
-			#mpirun -n 2 python scripts/stretch_fibrosis.py meshes_revisions/$mesh stretch_ss 1 $sc False
-			#mpirun -n 2 python scripts/stretch_fibrosis.py meshes_revisions/$mesh stretch_ff 1 $sc False 
-			#mpirun -n 2 python scripts/stretch_fibrosis.py meshes_revisions/$mesh stretch_ss 1 $sc False
-			#mesh="2d_mesh_replacement_N_10_k_${k}_seed_${seed}.h5"
-			##mpirun -n 2 python scripts/stretch_fibrosis.py meshes_revisions/$mesh stretch_ff 1 $sc False &
-			#mpirun -n 2 python scripts/stretch_fibrosis.py meshes_revisions/$mesh stretch_ss 1 $sc False
-			#mpirun -n 2 python scripts/stretch_fibrosis.py meshes_revisions/$mesh stretch_ff 1 $sc False &
-			#mpirun -n 2 python scripts/stretch_fibrosis.py meshes_revisions/$mesh stretch_ss 1 $sc False
+			mpirun -n 2 python scripts/stretch_fibrosis.py meshes_fibrosis_paper/$mesh stretch_ff 1 $sc False 
+			mpirun -n 2 python scripts/stretch_fibrosis.py meshes_fibrosis_paper/$mesh stretch_ss 1 $sc False
+			mpirun -n 2 python scripts/stretch_fibrosis.py meshes_fibrosis_paper/$mesh stretch_ff 1 $sc False 
+			mpirun -n 2 python scripts/stretch_fibrosis.py meshes_fibrosis_paper/$mesh stretch_ss 1 $sc False
+			mesh="2d_mesh_replacement_N_10_k_${k}_seed_${seed}.h5"
+			mpirun -n 2 python scripts/stretch_fibrosis.py meshes_fibrosis_paper/$mesh stretch_ff 1 $sc False &
+			mpirun -n 2 python scripts/stretch_fibrosis.py meshes_fibrosis_paper/$mesh stretch_ss 1 $sc False
+			mpirun -n 2 python scripts/stretch_fibrosis.py meshes_fibrosis_paper/$mesh stretch_ff 1 $sc False &
+			mpirun -n 2 python scripts/stretch_fibrosis.py meshes_fibrosis_paper/$mesh stretch_ss 1 $sc False
 		done
 	
 		for sc in 1.5 2 2.5 3 3.5 4
 		do
 			mesh="2d_mesh_interstitial_N_10_k_${k}_seed_${seed}.h5"
-			#mpirun -n 2 python scripts/stretch_fibrosis.py meshes_revisions/$mesh stretch_ff $sc 1 False 
-			#mpirun -n 2 python scripts/stretch_fibrosis.py meshes_revisions/$mesh stretch_ss $sc 1 False
-			#mpirun -n 2 python scripts/stretch_fibrosis.py meshes_revisions/$mesh stretch_ff $sc 1 False 
-			#mpirun -n 2 python scripts/stretch_fibrosis.py meshes_revisions/$mesh stretch_ss $sc 1 False
-			#mesh="2d_mesh_replacement_N_10_k_${k}_seed_${seed}.h5"
-			#mpirun -n 2 python scripts/stretch_fibrosis.py meshes_revisions/$mesh stretch_ff $sc 1 False &
-			#mpirun -n 2 python scripts/stretch_fibrosis.py meshes_revisions/$mesh stretch_ss $sc 1 False
-			#mpirun -n 2 python scripts/stretch_fibrosis.py meshes_revisions/$mesh stretch_ff $sc 1 False &
-			#mpirun -n 2 python scripts/stretch_fibrosis.py meshes_revisions/$mesh stretch_ss $sc 1 False
+			mpirun -n 2 python scripts/stretch_fibrosis.py meshes_fibrosis_paper/$mesh stretch_ff $sc 1 False 
+			mpirun -n 2 python scripts/stretch_fibrosis.py meshes_fibrosis_paper/$mesh stretch_ss $sc 1 False
+			mpirun -n 2 python scripts/stretch_fibrosis.py meshes_fibrosis_paper/$mesh stretch_ff $sc 1 False 
+			mpirun -n 2 python scripts/stretch_fibrosis.py meshes_fibrosis_paper/$mesh stretch_ss $sc 1 False
+			mesh="2d_mesh_replacement_N_10_k_${k}_seed_${seed}.h5"
+			mpirun -n 2 python scripts/stretch_fibrosis.py meshes_fibrosis_paper/$mesh stretch_ff $sc 1 False &
+			mpirun -n 2 python scripts/stretch_fibrosis.py meshes_fibrosis_paper/$mesh stretch_ss $sc 1 False
+			mpirun -n 2 python scripts/stretch_fibrosis.py meshes_fibrosis_paper/$mesh stretch_ff $sc 1 False &
+			mpirun -n 2 python scripts/stretch_fibrosis.py meshes_fibrosis_paper/$mesh stretch_ss $sc 1 False
 		done
 		
 		for sc in 3 4 5 6
 		do
 			mesh="2d_mesh_interstitial_N_10_k_${k}_seed_${seed}.h5"
-			#mpirun -n 2 python scripts/stretch_fibrosis.py meshes_revisions/$mesh stretch_ff $sc $sc False  
-			#mpirun -n 2 python scripts/stretch_fibrosis.py meshes_revisions/$mesh stretch_ss $sc $sc False
-			#mpirun -n 2 python scripts/stretch_fibrosis.py meshes_revisions/$mesh stretch_ff $sc $sc False 
-			#mpirun -n 2 python scripts/stretch_fibrosis.py meshes_revisions/$mesh stretch_ss $sc $sc False
-			#mesh="2d_mesh_replacement_N_10_k_${k}_seed_${seed}.h5"
-			#mpirun -n 2 python scripts/stretch_fibrosis.py meshes_revisions/$mesh stretch_ff $sc $sc False &
-			#mpirun -n 2 python scripts/stretch_fibrosis.py meshes_revisions/$mesh stretch_ss $sc $sc False
-			#mpirun -n 2 python scripts/stretch_fibrosis.py meshes_revisions/$mesh stretch_ff $sc $sc False &
-			#mpirun -n 2 python scripts/stretch_fibrosis.py meshes_revisions/$mesh stretch_ss $sc $sc False
+			mpirun -n 2 python scripts/stretch_fibrosis.py meshes_fibrosis_paper/$mesh stretch_ff $sc $sc False  
+			mpirun -n 2 python scripts/stretch_fibrosis.py meshes_fibrosis_paper/$mesh stretch_ss $sc $sc False
+			mpirun -n 2 python scripts/stretch_fibrosis.py meshes_fibrosis_paper/$mesh stretch_ff $sc $sc False 
+			mpirun -n 2 python scripts/stretch_fibrosis.py meshes_fibrosis_paper/$mesh stretch_ss $sc $sc False
+			mesh="2d_mesh_replacement_N_10_k_${k}_seed_${seed}.h5"
+			mpirun -n 2 python scripts/stretch_fibrosis.py meshes_fibrosis_paper/$mesh stretch_ff $sc $sc False &
+			mpirun -n 2 python scripts/stretch_fibrosis.py meshes_fibrosis_paper/$mesh stretch_ss $sc $sc False
+			mpirun -n 2 python scripts/stretch_fibrosis.py meshes_fibrosis_paper/$mesh stretch_ff $sc $sc False &
+			mpirun -n 2 python scripts/stretch_fibrosis.py meshes_fibrosis_paper/$mesh stretch_ss $sc $sc False
 		done	
 	done
 done
